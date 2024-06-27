f29044e1d6e52cd62b8ae8f53787c4ecb3026a0d
Pablo <pablomonardo@gmail.com>
2024-06-27 18:27:34 -0300

    misc: arreglo librerías fuera del .h

--- App/src/Juego/juego.h
+++ App/src/Juego/juego.h
@@ -1,11 +1,6 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-    #include "../Utilidades/utilidades.h"
-
-    #include <stdio.h>
-    #include <stdlib.h>
-
     #define ARCHIVO_CONFIG  "config.txt"
     #define ARCHIVO_INFORME "../../Resultados/"
 
