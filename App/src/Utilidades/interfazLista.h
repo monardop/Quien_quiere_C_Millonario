eb98d6d69900d9d1da04905ee1444e74082ea222
Pablo <pablomonardo@gmail.com>
2024-06-27 18:23:19 -0300

    Creo un header para la Lista

---
+++ App/src/Utilidades/interfazLista.h
@@ -0,0 +1,30 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+    typedef struct _nodo 
+    {
+        void *data;
+        unsigned dataSize;
+        struct _nodo *next;
+    }tNodo;
+
+    typedef tNodo *dsLista;
+    typedef int  (*cmp)(const void *, const void *);
+    typedef void (*lambda)(void *);
+    typedef void (*reduceFunc)(void *container, void *); 
+
+    // Funciones generales
+    void nuevaLista(dsLista *lista);
+    void vaciarLista(dsLista *lista);
+    int  listaVacia(dsLista *lista);
+    int  agregarElemento(dsLista *lista, void *data, unsigned dataSize);
+    int  agregarPosicionN(dsLista *lista, void *data, unsigned dataSize, int pos);
+
+    // Funciones particulares para la app
+    int  algoritmoFisherYates(dsLista *lista, unsigned ce);
+    void listMap(dsLista *lista, lambda func);
+    void listFilter(dsLista *lista, cmp cmp);
+    void listReduce(dsLista *lista, void* container); 
+    void imprimirLista(dsLista *lista, lambda print);
+
+#endif
