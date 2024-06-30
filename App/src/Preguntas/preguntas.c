b178144c163396356ce53a0855b88443fa9c8a67
Pablo <pablomonardo@gmail.com>
2024-06-30 17:50:48 -0300

    feat: agrego función de parseo del JSON

--- App/src/Preguntas/preguntas.c
+++ App/src/Preguntas/preguntas.c
@@ -1,7 +1,45 @@
 #include "preguntas.h"
 
+char *copiarString(char *destino, char *origen)
+{
 
-int obtenerPreguntas()
+    while (*origen != '"')
+    {
+        *destino = *origen;
+        destino++;
+        origen++;
+    }
+    *(destino + 1) = '\0';
+
+    return (origen + 1);
+}
+
+int parseoPreguntas(dsLista *preguntas, char *jsonPreguntas)
+{
+    char dificultad;
+    tPregunta aux;
+
+    while((jsonPreguntas = strchr(jsonPreguntas, '{')) != NULL)
+    {
+        jsonPreguntas = copiarString(aux.pregunta, strchr(jsonPreguntas, ':') + 2);
+        jsonPreguntas = copiarString(aux.resp_correcta, strchr(jsonPreguntas, ':') + 2);
+        jsonPreguntas = copiarString(aux.opcion_1, strchr(jsonPreguntas, ':') + 2);
+        jsonPreguntas = copiarString(aux.opcion_2, strchr(jsonPreguntas, ':') + 2);
+        jsonPreguntas = copiarString(aux.opcion_3, strchr(jsonPreguntas, ':') + 2);
+        jsonPreguntas = strchr(jsonPreguntas, ':') + 1;
+        dificultad = *jsonPreguntas;
+        aux.nivel = atoi(&dificultad);
+        jsonPreguntas = copiarString(aux.id, strchr(jsonPreguntas, ':') + 2);
+
+        if(agregarElemento(preguntas, (void*)&aux, sizeof(tPregunta)) != OK)
+            return FALLA_MEMORIA;
+        
+    }
+
+    return OK;
+}
+
+int obtenerPreguntas(dsLista *preguntas)
 {
     tRespuesta respuesta;
 
@@ -11,7 +49,13 @@ int obtenerPreguntas()
         return FALLA_CURL;
     }
 
-    // Acá proceso las preguntas
+    if (parseoPreguntas(preguntas, respuesta.string) != OK)
+    {
+        vaciarLista(preguntas);
+        free(respuesta.string);
+        return FALLA_MEMORIA;
+    }
+
     free(respuesta.string);
     return OK;
 }
--- App/src/Preguntas/preguntas.h
+++ App/src/Preguntas/preguntas.h
@@ -1,6 +1,11 @@
 #ifndef PREGUNTAS_H
 #define PREGUNTAS_H
 
-  
+    #include "../Utilidades/utilidades.h"
+    #include "../Utilidades/interfazLista.h"
+
+
+    int obtenerPreguntascURL(tRespuesta *respuesta);
+    int obtenerPreguntas(dsLista *preguntas);
 
 #endif
