e5a9ae85a195536475929d86c6b9378a5f970312
Pablo <pablomonardo@gmail.com>
2024-06-30 18:52:08 -0300

    misc: se agregan firmas

--- App/src/Preguntas/preguntas.h
+++ App/src/Preguntas/preguntas.h
@@ -5,7 +5,9 @@
     #include "../Utilidades/interfazLista.h"
 
 
-    int obtenerPreguntascURL(tRespuesta *respuesta);
-    int obtenerPreguntas(dsLista *preguntas);
+    int  obtenerPreguntascURL(tRespuesta *respuesta);
+    int  obtenerPreguntas(dsLista *preguntas);
+    int  preguntasPartida(dsLista *preguntas, dsLista *partidaActual, int rounds, int dificultad);
+    void mostrarPregunta(dsLista *listaPreguntas, const char respuestaCorrecta);
 
 #endif
